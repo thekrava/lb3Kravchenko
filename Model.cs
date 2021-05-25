4d7cb186bbf7eab09c6a992fc9d46d66498b695a
Кравченко Дмитро <cravchenko5@ukr.net>
2021-05-25 14:21:21 +0300

    Добавьте файлы проекта.

---
+++ Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace A1_StopWatch
+{
+    class Model
+    {
+        readonly Stopwatch stopwatch = new();
+        bool _prevstate = false;
+        public bool Start()
+        {
+            if (stopwatch.IsRunning)
+            {
+                stopwatch.Stop();
+                return false;
+            }
+            else
+            {
+                stopwatch.Start();
+                return true;
+            }
+        }
+        public void Reset()
+        {
+            if (stopwatch.IsRunning)
+                stopwatch.Restart();
+            else
+                stopwatch.Reset();
+        }
+        public (TimeSpan, bool?) Read() =>
+         (stopwatch.Elapsed, stopwatch.IsRunning == _prevstate ? null : (_prevstate = stopwatch.IsRunning));
+    }
+}
---
+++ Presenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace A1_StopWatch
+{
+    class Presenter
+    {
+        readonly Presenter model = new();
+        event EventHandler<(TimeSpan, bool?)> Update;
+        readonly DispatcherTimer timer = new();
+        public Presenter(EventHandler<(TimeSpan, bool?)> updateevent)
+        {
+            Update = updateevent;
+            UpdateStopwatch(this, null);
+            timer.Interval = new TimeSpan(100000);
+            timer.Tick += UpdateStopwatch;
+        }
+        public void StartStopwatch()
+        {
+            timer.IsEnabled = model.Start();
+            UpdateStopwatch(this, null);
+        }
+        public void ResetStopwatch()
+        {
+            model.Reset();
+            UpdateStopwatch(this, null);
+        }
+        void UpdateStopwatch(object sender, EventArgs e) => Update?.Invoke(this, model.Read());
+    }
+}
